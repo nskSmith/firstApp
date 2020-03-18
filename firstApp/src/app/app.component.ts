cbd00b7263a39a424e11c811a30647ccb34eb109
nskSmith <nsk.kovalev@gmail.com>
2020-03-18 17:49:15 +0700

    ToDo-2 Task list

--- firstApp/src/app/app.component.ts
+++ firstApp/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstApp';
+    title = 'First App';
+    date = Date() 
 }
---
+++ firstApp/src/components/todo/task.ts
@@ -0,0 +1,7 @@
+export class Task {
+    constructor (
+        public date: string,
+        public text: string,
+        public status: string
+    ) { }
+}
