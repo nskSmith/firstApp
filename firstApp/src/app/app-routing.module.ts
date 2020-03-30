6301ea20d0cc74a0fb5637e021bc5b86069faee8
nskSmith <nsk.kovalev@gmail.com>
2020-03-30 19:39:55 +0700

    feat(): TODO-3 Add page for TODO item creation
    Create todo item component and new page for this

--- firstApp/src/app/app-routing.module.ts
+++ firstApp/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ToDoComponent } from 'src/components/todo/todo.component'
+import { CreateTodoComponent } from 'src/components/create-todo/create-todo.component';
 
 
 const routes: Routes = [
@@ -8,6 +9,11 @@ const routes: Routes = [
     path:'todo',
     component: ToDoComponent,
     data: { title: 'TODO List'}
+  },
+  {
+    path:'item',
+    component: CreateTodoComponent,
+    data: { title: 'Create TODO Item'}
   }
 ];
 
